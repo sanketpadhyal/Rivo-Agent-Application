0e3beebb12e69cd37e8935289aeb8c019e15f6a4
Sᴀɴᴋᴇᴛ 🖤 <mrsanketpadhyal@gmail.com>
2026-09-18 12:32:37 +0530

    Create sanket.cpp

---
+++ sanket.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int p = 1000;
+    int r = 5;
+    int t = 2;
+    int si;
+
+    si = (p * r * t) / 100;
+
+    printf("Simple Interest = %d", si);
+
+    return 0;
+}
